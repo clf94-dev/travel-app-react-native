7a96bde2206dc88b4da365116cb0c783d4da1bdf
clf94-dev <clf94@hotmail.es>
2021-02-13 07:54:28 +0100

    created simple bottom bar

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
+import * as React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+import HomeScreen from './screens/HomeScreen'
+import SearchScreen from './screens/SearchScreen'
+import SettingsScreen from './screens/SettingsScreen'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={HomeScreen} />
+        <Tab.Screen name='Search' component={SearchScreen}/>
+        <Tab.Screen name='Settings' component={SettingsScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+    </SafeAreaProvider>
+    
   );
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
---
+++ screens/SettingsScreen.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import {View, Text} from 'react-native';
+
+export default function SettingsScreen(){
+    return(
+    
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Settings screen</Text>
+        </View>
+        
+    )
+}
